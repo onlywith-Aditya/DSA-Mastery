07cb5b33eff96d0fff3d1c6e3ee0d3f8eea94475
Aditya Dadhich <adityadadhichofficial@gmail.com>
2026-07-05 17:20:47 +0530

    Time: 153 ms (7.55%), Space: 72.8 MB (50.86%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,7 +1,51 @@
+import java.util.Collections;
+
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        // 1st-Way=> Sorting (O(nlogn))
+
+            // Arrays.sort(nums);
+            // return nums[nums.length - k];
+
+        // Heap=> Min Heap O(n log k)
+
+            // PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+            //         // Add last k element in heap.
+
+            // for(int i = 0 ; i < nums.length;i++){
+                    
+            //         // Add current element to heap.
+
+            //     minHeap.offer(nums[i]);
+
+            //         // If heap size exceed k, remove the smallest element.
+
+            //     if(minHeap.size() > k){
+            //         minHeap.poll();
+            //     }
+
+
+
+            // } 
+            // return minHeap.peek();
+
+
+        // MaxHeap
+
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+                // Add all things to Heap.
+            
+            for(int  i = 0;i<nums.length;i++){
+                maxHeap.offer(nums[i]);
+            }
+                // Normal for loop to remove k-1 all element.
+            for(int i = 0;i<k-1;i++){
+                maxHeap.poll();
+            }
+
+            return maxHeap.peek();       
     }
 }
