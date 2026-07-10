fd27d71db61aaf8219d242c316d2d242741ea07c
Aditya Dadhich <adityadadhichofficial@gmail.com>
2026-07-10 14:09:17 +0530

    Time: 0 ms (100%), Space: 43.1 MB (27.22%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -1,38 +1,53 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+        
+//         if(root == null){
+//             return  null;
+//         }
+//         swap(root);
+//         invertTree(root.left);
+//         invertTree(root.right);
+
+//         return root;
+//     }
+
+//     private void swap(TreeNode node){
+//         if (node == null) {
+//             return;
+//         }
+//         TreeNode temp = node.left;
+//         node.left = node.right;
+//         node.right = temp;
+//     }
+
+// }
+
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
         
-        if(root == null){
-            return  null;
-        }
-        swap(root);
-        invertTree(root.left);
-        invertTree(root.right);
-
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        
+        root.left = right;
+        root.right = left;
+        
         return root;
     }
-
-    private void swap(TreeNode node){
-        if (node == null) {
-            return;
-        }
-        TreeNode temp = node.left;
-        node.left = node.right;
-        node.right = temp;
-    }
-
 }
