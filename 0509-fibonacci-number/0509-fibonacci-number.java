5c315b8893b8014c13ad4f4e8808e133fddebd1f
Aditya Dadhich <adityadadhichofficial@gmail.com>
2026-09-02 12:41:32 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.63%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,27 @@
 class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
+
     public int fib(int n) {
-        if(n == 0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
-    }
+    // 1. Recursion  
+        // if(n == 0){
+        //     return 0;
+        // }
+        // if(n==1){
+        //     return 1;
+        // }
+        // return fib(n-1)+fib(n-2);
+
+    // 2. DP
+       if(n<=1) return n;
+
+       // Check if already computed.
+       if(memo.containsKey(n)){
+        return memo.get(n);
+       }
+
+       // Compute and Store
+       int result = fib(n-1) + fib(n-2);
+       memo.put(n,result);
+       return result;
+}
 }
