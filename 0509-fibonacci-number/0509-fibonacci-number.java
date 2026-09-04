72b9fa39285d427af46d5c9d6576c9f69bdf2ed9
Aditya Dadhich <adityadadhichofficial@gmail.com>
2026-09-04 13:45:10 +0530

    Time: 0 ms (100%), Space: 42 MB (70.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    HashMap<Integer, Integer> memo = new HashMap<>();
+    // HashMap<Integer, Integer> memo = new HashMap<>();
 
     public int fib(int n) {
     // 1. Recursion  
@@ -12,16 +12,33 @@ class Solution {
         // return fib(n-1)+fib(n-2);
 
     // 2. DP
-       if(n<=1) return n;
+    //    if(n<=1) return n;
 
-       // Check if already computed.
-       if(memo.containsKey(n)){
-        return memo.get(n);
-       }
+    //    // Check if already computed.
+    //    if(memo.containsKey(n)){
+    //     return memo.get(n);
+    //    }
 
-       // Compute and Store
-       int result = fib(n-1) + fib(n-2);
-       memo.put(n,result);
-       return result;
+    //    // Compute and Store
+    //    int result = fib(n-1) + fib(n-2);
+    //    memo.put(n,result);
+    //    return result;
+
+    // 3. Tabulization with Space Optimization
+    if(n == 0) return 0;
+    if(n==1) return 1;
+        int prev =1;
+        int prev_prev = 0;
+        int ans = 0;
+        for(int i = 2 ; i <= n; i++){
+            ans = prev + prev_prev;
+            prev_prev = prev;
+            prev = ans;
+        }
+        return ans;
+
+
+
+
 }
 }
